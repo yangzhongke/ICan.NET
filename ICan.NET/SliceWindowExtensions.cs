82648f3ed03259545377766361ea80b0144b52b9
yangzhongke <yzk365@qq.com>
2022-05-18 10:00:26 +0800

    Window

---
+++ ICan.NET/SliceWindowExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class SliceWindowExtensions
+    {
+        public static IEnumerable<T[]> Window5<T>(this T[] items, int windowSize)
+        {
+            if(windowSize<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize),"windowsSize must be positive");
+            }
+            if(windowSize>items.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), "windowsSize must not be greater than items.Length");
+            }
+            int head = 0;
+            int tail = windowSize-1;
+            while(tail<items.Length)
+            {
+                T[] windowItems = new T[windowSize];
+                for (int i = 0; i < windowSize; i++)
+                {
+                    windowItems[i] = items[head+i];
+                }
+                yield return windowItems;
+                head++;
+                tail++;
+            }
+        }
+
+        public static IEnumerable<T[]> Window<T>(this IList<T> items, int windowSize)
+        {
+            if (windowSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), "windowsSize must be positive");
+            }
+            int count = items.Count;
+            if (windowSize > count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), "windowsSize must not be greater than items.Count");
+            }
+            int head = 0;
+            int tail = windowSize - 1;
+            while (tail < count)
+            {
+                T[] windowItems = new T[windowSize];
+                for (int i = 0; i < windowSize; i++)
+                {
+                    windowItems[i] = items[head + i];
+                }
+                yield return windowItems;
+                head++;
+                tail++;
+            }
+        }
+
+
+        public static IEnumerable<T[]> Window<T>(this IEnumerable<T> items,int windowSize)
+        {
+            if (windowSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), "windowsSize must be positive");
+            }
+            int count = items.Count();
+            if (windowSize > count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), "windowsSize must not be greater than items.Count");
+            }
+            int head = 0;
+            int tail = windowSize - 1;
+            while (tail < count)
+            {
+                T[] windowItems = new T[windowSize];
+                for (int i = 0; i < windowSize; i++)
+                {
+                    windowItems[i] = items.ElementAt(head + i);
+                }
+                yield return windowItems;
+                head++;
+                tail++;
+            }
+        }
+    }
+}
---
+++ ICanNetConsoleTest/Program.cs
@@ -0,0 +1,49 @@
+﻿int[] items = { 3,1,4,1,5,9};
+foreach(var w in items.Window(2))
+{
+    WriteLine(w);
+}
+Console.WriteLine("**********");
+foreach (var w in items.Window(5))
+{
+    WriteLine(w);
+}
+Console.WriteLine("**********");
+foreach (var w in items.Window(6))
+{
+    WriteLine(w);
+}
+Console.WriteLine("**********");
+
+List<int> items2 = new List<int> { 3, 1, 4, 1, 5, 9 };
+foreach (var w in items2.Window(2))
+{
+    WriteLine(w);
+}
+Console.WriteLine("**********");
+foreach (var w in items2.Window(5))
+{
+    WriteLine(w);
+}
+Console.WriteLine("**********");
+foreach (var w in items2.Window(6))
+{
+    WriteLine(w);
+}
+
+/*
+Console.WriteLine("**********");
+foreach (var w in items.Window(7))
+{
+    WriteLine(w);
+}*/
+
+
+void WriteLine<T>(T[] items)
+{
+    foreach (var e in items)
+    {
+        Console.Write($"{e} ");
+    }
+    Console.WriteLine();
+}
